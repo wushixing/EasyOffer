36bb9228d8d7c9c43058fecdd3f4f4fe155b3673
吴世兴 <wu.shixing123@163.com>
2018-04-03 10:49:59 +0800

    项目基本搭建

---
+++ EasyOffer/EasyOffer/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  EasyOffer
+//
+//  Created by 吴世兴 on 2018/4/1.
+//  Copyright © 2018年 wushixing. All rights reserved.
+//
+
+import Foundation
+
+//SortCell ProjectCell UnitCell AmountCell UnitPrice TotalSrice Illustrate
